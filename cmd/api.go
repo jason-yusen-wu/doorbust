8b42e6cec20e97b663f4afe79b873918a5dce142
senyu <jasipoo@proton.me>
2026-08-16 16:03:03 -0500

    scaffolding for mounting and running api

--- cmd/api.go
+++ cmd/api.go
@@ -1,1 +1,57 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	// middleware
+	r.Use(middleware.RequestID) // rate limiting & pass request in context
+	r.Use(middleware.RealIP)    // rate limiting, analytics & tracing
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("all good"))
+	})
+
+	return r
+}
+
+func (app *application) run(h http.Handler) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      h,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("Server has started at addr %s", app.config.addr)
+	// TODO: graceful shutdown script
+
+	return srv.ListenAndServe()
+}
+
+// global struct
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string // network address where server is hosted
+	db   dbConfig
+}
+
+type dbConfig struct {
+	dsn string // data source name for connecting to DB
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"log/slog"
+	"os"
+)
 
+func main() {
+	cfg := config{
+		addr: ":8080",
+		db:   dbConfig{},
+	}
+
+	api := application{
+		config: cfg,
+	}
+
+	// structured (text based) logger as global logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	if err := api.run(api.mount()); err != nil {
+		slog.Error("Server has failed to start", "error", err)
+		os.Exit(1)
+	}
 }
